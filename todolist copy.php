c59973cbd8ea0b0bd6338cb2126a3a18d27a487c
AndreiV <vlaicu.andrei@yahoo.com>
2023-04-30 21:02:29 +0300

    Finish design

--- todolist copy.php
+++ todolist copy.php
@@ -16,9 +16,8 @@ if (isset($_SESSION['login_status']) && $_SESSION['login_status'] == true) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To do list</title>
 </head>
 <body>
